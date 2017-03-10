831b73fd5eec4948b82881f604d8c49cd6d91fe1
Michael Reynolds <reynoldsm88@gmail.com>
2017-03-10 17:40:49 -0500

    Part 3 - stub out POST method for API

--- credit-approvals-service/src/main/scala/org/kda/credit/service/CreditService.scala
+++ credit-approvals-service/src/main/scala/org/kda/credit/service/CreditService.scala
@@ -3,7 +3,7 @@ package org.kda.credit.service
 import akka.actor.Actor
 import spray.httpx.SprayJsonSupport
 import spray.routing.{HttpService, Route}
-import org.kda.credit.model.{ApprovedApplication, CreditLine, Customer, UnapprovedApplication}
+import org.kda.credit.model._
 import spray.httpx.SprayJsonSupport._
 
 class CreditApprovalsServiceActor extends Actor with CreditApprovalsService {
@@ -19,18 +19,28 @@ class CreditApprovalsServiceActor extends Actor with CreditApprovalsService {
 trait CreditApprovalsService extends HttpService with SprayJsonSupport {
 
   import spray.http.MediaTypes._
-  import org.kda.credit.model.{ApprovedApplication, CreditLine, Customer}
+  import org.kda.credit.model.{ApprovedApplication, PendingApplication, CreditLine, Customer}
   import org.kda.credit.service.CreditApprovalJSonProtocol._
 
   //@formatter:off
   val applicationRoute: Route =
     path( "credit-approvals" ) {
       get {
-        respondWithMediaType( `application/json` )
-        val customer = Customer( "123", 600 )
-        val credit = CreditLine( customer, BigDecimal( 100 ) )
         complete {
-          ApprovedApplication( customer.id, Some( credit ) )
+          PendingApplication( "123" ) // you can POST this payload to the service to convert it into an approved application
+        }
+      } ~
+      post {
+        respondWithMediaType( `application/json` )
+        entity( as[ PendingApplication ] ) { application =>
+          detach() {
+            // in Part 4 this code will be integrated with the rules engine to actually perform the business logic
+            val customer = Customer( application.customerId, 400 )
+            val credit = CreditLine( customer, BigDecimal( 5000 ) )
+            complete {
+              ApprovedApplication( customer.id, Some( credit ) )
+            }
+          }
         }
       }
     }
